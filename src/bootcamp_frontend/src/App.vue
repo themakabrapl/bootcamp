95a35387e84c0cf9363630afd5f1b54429943889
themakabrapl <Gtadebil@gmail.com>
2024-06-24 19:39:33 +0200

    Commit

---
+++ src/bootcamp_frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <img src="/logo2.svg" alt="DFINITY logo" />
+    Siema!
+    <br/>
+    <button @click="podbij">podbij</button>
+      {{ licznik }}
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      licznik: 0
+    }
+  },
+  methods: {
+    podbij() {
+      this.licznik++
+    }
+  }
+}
+</script>
